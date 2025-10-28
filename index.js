852e2db52b76eb3fd9eaf165e749b8cd453e86ae
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-28 18:15:14 +0000

    else

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function calculateAward(points) {
+    if (points > 99) {
+        return "First Place";
+    } else if (points > 49 && points < 100) {
+        return "Second Place";
+    } else {
+        return "Participation Award";
+    }
+}
